b748d9d0d66e8849465df0cf5752298953e4a603
Nicholas Berlette <nick@berlette.com>
2026-02-09 17:16:51 -0800

    refactor: update version bump script

--- scripts/bump_version.ts
+++ scripts/bump_version.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno -Aq --unstable-net
 
-// deno-lint-ignore-file no-import-prefix no-console no-explicit-any
+// deno-lint-ignore-file no-import-prefix no-explicit-any
 
 import process from "node:process";
 import { $ } from "jsr:@david/dax@0.44.1";
@@ -8,21 +8,44 @@ import * as semver from "jsr:@std/semver@1";
 
 import pkg from "../deno.json" with { type: "json" };
 
-let releaseType: semver.ReleaseType | null = null;
-let newVersion: semver.SemVer | null = null;
+type SemVer = semver.SemVer;
+type ReleaseType = semver.ReleaseType;
+type VersionLike = string | semver.SemVer | Version;
 
-function semverToString(this: semver.SemVer) {
-  return semver.format(this);
+interface Version extends SemVer {
+  increment(releaseType: ReleaseType): Version;
+  toString(prefix?: string): string;
 }
 
-function parse(version: string): semver.SemVer {
-  const parsed = semver.parse(version);
-  parsed.toString = semverToString;
-  return parsed;
+let releaseType: ReleaseType | null = null;
+let newVersion: Version | null = null;
+
+function semverToString(this: Version, prefix?: string) {
+  return (prefix ?? "") + semver.format(this);
+}
+
+function parse(version: VersionLike): Version {
+  if (typeof version === "string") {
+    version = semver.parse(version) as Version;
+  }
+  version.toString = semverToString.bind(version as Version);
+  if (!("increment" in version)) {
+    const v = version as Version;
+    v.increment = increment.bind(version);
+  }
+  return version as Version;
+}
+
+function increment(this: VersionLike, releaseType: ReleaseType): Version {
+  return parse(
+    semver.increment(
+      typeof this === "string" ? semver.parse(this) : this,
+      releaseType,
+    ),
+  );
 }
 
 const oldVersion = parse(pkg.version);
-oldVersion.toString = semverToString;
 
 const releaseTypes = [
   "pre",
@@ -35,8 +58,7 @@ const releaseTypes = [
   "prerelease",
 ] as const;
 
-const isReleaseType = (s: any): s is semver.ReleaseType =>
-  releaseTypes.includes(s);
+const isReleaseType = (s: any): s is ReleaseType => releaseTypes.includes(s);
 
 const arg = process.argv[2];
 if (isReleaseType(arg)) {
@@ -46,7 +68,8 @@ if (isReleaseType(arg)) {
 } else if (semver.canParse(arg)) {
   newVersion = parse(arg);
   if (!semver.greaterThan(newVersion, oldVersion)) {
-    console.error(
+    $.logError(
+      "error",
       `New version ${newVersion} is not greater than old version ${oldVersion}`,
     );
     process.exit(1);
@@ -54,28 +77,76 @@ if (isReleaseType(arg)) {
   releaseType = null;
 }
 
-if (releaseType) {
-  newVersion = semver.increment(oldVersion, releaseType);
+if (isReleaseType(releaseType)) {
+  newVersion = oldVersion.increment(releaseType);
 }
 
 if (!newVersion && process.stdin.isTTY) {
-  const options = [...releaseTypes];
-  const input = await $.select({
-    message: `Select release type (current: ${oldVersion})`,
+  let options: string[] | (readonly [string, string])[] = [
+    [`${oldVersion}`, `(no change)`],
+    ...releaseTypes.map((t) =>
+      [`${oldVersion.increment(t)}`, `(${t})`] as const
+    ),
+    [`_._._`, `(enter manually)`],
+  ];
+  const maxLeftLen = Math.max(...options.map(([s]) => s.length + 3));
+  options = options.map(([l, r]) =>
+    `${l} ${".".repeat(Math.max(0, maxLeftLen - l.length))} ${r}`
+  );
+  const input = await $.maybeSelect({
+    message:
+      `Select next version by SemVer release type (current: ${oldVersion})`,
     options,
-    initialIndex: options.indexOf("patch"),
+    initialIndex: 0,
     noClear: true,
   });
-  releaseType = options[input];
-  newVersion = semver.increment(oldVersion, releaseType);
+  if (input) {
+    if (input === options.length - 1) {
+      const custom = await $.maybePrompt({
+        message: `Enter a new semantic version number (current: ${oldVersion})`,
+        default: oldVersion.toString(),
+        noClear: true,
+      });
+
+      if (custom) {
+        if (semver.canParse(custom)) {
+          newVersion = parse(custom);
+        } else {
+          $.logError("error", `Invalid semantic version number: ${custom}`);
+          process.exit(2);
+        }
+      }
+    } else {
+      const value = releaseTypes[input - 1];
+      if (isReleaseType(value)) {
+        newVersion = oldVersion.increment(value);
+      }
+    }
+  } else {
+    $.logWarn("Skipping version increment per user request.");
+    process.exit(0);
+  }
 }
 
 if (!newVersion) {
-  console.error("No valid release type or version specified.");
+  $.logError("error", "No valid release type or version specified.");
   process.exit(1);
 }
 
-newVersion.toString = semverToString;
+$.logStep(`Preparing to bump version to ${newVersion}...`);
+
+const proceed = await $.maybeConfirm(
+  `Proceed to update deno.json, package.json, and Cargo.toml?`,
+  {
+    default: true,
+    noClear: true,
+  },
+);
+
+if (!proceed) {
+  $.logWarn("Skipping configuration update step. No files changed!");
+  process.exit(0);
+}
 
 const ROOT_DIR = $.path("./").resolve();
 const DENO_JSON = ROOT_DIR.join("deno.json");
@@ -86,7 +157,7 @@ denoJson = denoJson.replace(
   `${newVersion}`,
 );
 await DENO_JSON.writeText(denoJson);
-$.logStep("✔︎ bumped", `deno.json to version ${newVersion}`);
+$.logStep("✔︎ bumped", `deno.json to ${newVersion}`);
 
 const NPM_DIR = ROOT_DIR.join("npm").resolve();
 const PKG_JSON = NPM_DIR.join("package.json");
@@ -94,7 +165,7 @@ const PKG_JSON = NPM_DIR.join("package.json");
 let pkgJson = await PKG_JSON.readText();
 pkgJson = pkgJson.replace(/(?<="version"\s*:\s*")(.+?)(?=")/, `${newVersion}`);
 await PKG_JSON.writeText(pkgJson);
-$.logStep("✔︎ bumped", `package.json to version ${newVersion}`);
+$.logStep("✔︎ bumped", `package.json to ${newVersion}`);
 
 const RUST_DIR = ROOT_DIR.join("rs_lib").resolve();
 const CARGO_TOML = RUST_DIR.join("Cargo.toml");
@@ -105,7 +176,7 @@ cargoToml = cargoToml.replace(
   `${newVersion}`,
 );
 await CARGO_TOML.writeText(cargoToml);
-$.logStep("✔︎ bumped", `Cargo.toml to version ${newVersion}`);
+$.logStep("✔︎ bumped", `Cargo.toml to ${newVersion}`);
 
 $.logLight("Updating cargo lockfile ...");
 await $`cargo update -p dawm --manifest-path ${CARGO_TOML.toString()}`;
